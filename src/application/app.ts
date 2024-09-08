b31a7e298accea78d9d10d3be62199e62eb833a8
Vevey Zhan <vzhan00@gmail.com>
2024-09-08 00:16:03 -0400

    adding cors

--- src/application/app.ts
+++ src/application/app.ts
@@ -1,10 +1,15 @@
 import express from "express";
+
 import router from "./router"
 import bodyParser from 'body-parser';
 import watchlistRouter from './routers/watchlistRouter'
 
 
 const app = express();
+
+var cors = require('cors')
+app.use(cors());
+
 app.use(bodyParser.json());
 
 app.use('/', router);
